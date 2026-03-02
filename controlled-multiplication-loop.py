9f6fc18567c37ed1fd4aaf5a941d66ce33dd3a0b
steamworks <talhah.ahmad88@gmail.com>
2026-03-01 22:59:28 -0700

    Update controlled-multiplication-loop.py

--- controlled-multiplication-loop.py
+++ controlled-multiplication-loop.py
@@ -1,12 +1,12 @@
 # store variables
 threshold = 100
 product = 1
-currentNumber = 1
+currentNumber = 0
 
 # repeatedly multiply product with consecutive integers until product is greater than threshold
 while (product <= threshold):
-    product *= currentNumber
     currentNumber += 1
+    product *= currentNumber
 
 # print out final product and the final integer
 print(f"The final product is: {product}")
